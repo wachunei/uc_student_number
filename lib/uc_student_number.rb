26561ed239229791f99ed1dca7b6e8807cd8e5d1
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2016-04-05 17:18:30 -0300

    Include algorithm and tests

--- lib/uc_student_number.rb
+++ lib/uc_student_number.rb
@@ -1,5 +1,25 @@
-require "uc_student_number/version"
+require 'uc_student_number/version'
 
+# UcStundentMo dule
 module UcStudentNumber
-  # Your code goes here...
+  def self.validate(number)
+    return false if !number || number.to_s.length < 7
+    number = number.to_s.strip
+    given_digits = number.reverse[1..-1].split('').map(&:to_i)
+    given_verification = number.reverse[0].upcase
+    get_verification_digit(given_digits) == given_verification
+  end
+
+  def self.get_verification_digit(digits)
+    counter = 2
+    sum = 0
+    digits.each do |digit|
+      sum += (digit * counter)
+      counter += 1
+      counter = counter > 8 ? 2 : counter
+    end
+
+    ((11 - sum) % 11 != 10) ? ((11 - sum) % 11).to_s : 'J'
+  end
+  private_class_method :get_verification_digit
 end
--- spec/uc_student_number_spec.rb
+++ spec/uc_student_number_spec.rb
@@ -5,7 +5,39 @@ describe UcStudentNumber do
     expect(UcStudentNumber::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'validates a student number as integer' do
+    student_numbers = [10_637_419, 8_630_739]
+    student_numbers.each do |student_number|
+      expect(UcStudentNumber.validate(student_number)).to be true
+    end
+  end
+
+  it 'validates a student number as string' do
+    student_numbers = %w(10637419 08630739)
+    student_numbers.each do |student_number|
+      expect(UcStudentNumber.validate(student_number)).to be true
+    end
+  end
+
+  it 'doesn\'t validate a short student number integer' do
+    student_number = 10_637
+    expect(UcStudentNumber.validate(student_number)).to be false
+  end
+
+  it 'doesn\'t validate a short student number string' do
+    student_number = '10637'
+    expect(UcStudentNumber.validate(student_number)).to be false
+  end
+
+  it 'doesn\'t validate a nil student number' do
+    student_number = nil
+    expect(UcStudentNumber.validate(student_number)).to be false
+  end
+
+  it 'doesn\'t validate a boolean student number' do
+    student_numbers = [true, false]
+    student_numbers.each do |student_number|
+      expect(UcStudentNumber.validate(student_number)).to be false
+    end
   end
 end
